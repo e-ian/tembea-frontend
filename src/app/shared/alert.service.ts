814416bc5a43fbf7fb9124f2e6f2767eedf20da8
ah-med <ahmedomosanya@gmail.com>
2019-03-05 13:41:27 +0100

    feat(alerts): notification & alerts consistency frontend
    - add alert icons
    - add toastr style
    - change alert positioning to top center
    
    [WIP #164355018]

--- src/app/shared/alert.service.ts
+++ src/app/shared/alert.service.ts
@@ -5,15 +5,23 @@ import { TOASTR_TOKEN, Toastr } from 'src/app/shared/toastr.service';
 export class AlertService {
   constructor(@Inject(TOASTR_TOKEN) private toastr: Toastr) { }
   success(msg: string, title?: string) {
-    this.toastr.success(msg, title);
+    this.toastr.success(msg, title, {
+      positionClass: 'toast-top-center'
+    });
   }
   info(msg: string, title?: string) {
-    this.toastr.info(msg, title);
+    this.toastr.info(msg, title, {
+      positionClass: 'toast-top-center'
+    });
   }
   warning(msg: string, title?: string) {
-    this.toastr.warning(msg, title);
+    this.toastr.warning(msg, title, {
+      positionClass: 'toast-top-center'
+    });
   }
   error(msg: string, title?: string) {
-    this.toastr.error(msg, title);
+    this.toastr.error(msg, title, {
+      positionClass: 'toast-top-center'
+    });
   }
 }
