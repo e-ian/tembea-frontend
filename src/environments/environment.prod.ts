b41c19bb2bf97f6a2b9f17ce01801c95edbb01a4
Mubarak Imam <mubarak.imam@andela.com>
2019-03-13 09:58:16 +0100

    fix(deploy): fix missing env variables
    - added GMapsAPiKey and BugsnagAPI key to prod and staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   tembeaFrontEndUrl: 'https://tembea.andela.com',
   andelaAuthServiceUrl: 'https://api-prod.andela.com/login?redirect_url',
   teamUrl: 'andela.slack.com',
-  googMapsAPIKey: ''
+  googMapsAPIKey: 'AIzaSyDT1bJDksmdUa0221cLtjq36p9GidNa110',
+  BUGSNAG_API_KEY: '710805426cc2698759ff88cdcbc91a0c'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -8,7 +8,8 @@ export const environment = {
   tembeaFrontEndUrl: 'https://tembea-staging.andela.com',
   andelaAuthServiceUrl: 'https://api-staging.andela.com/login?redirect_url',
   teamUrl: 'andela-tembea.slack.com',
-  googMapsAPIKey: ''
+  googMapsAPIKey: 'AIzaSyDT1bJDksmdUa0221cLtjq36p9GidNa110',
+  BUGSNAG_API_KEY: '710805426cc2698759ff88cdcbc91a0c'
 };
 
 /*
