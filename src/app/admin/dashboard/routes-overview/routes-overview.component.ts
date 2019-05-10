0a4294d39a710130cee61945d1991a8ed7856685
Kizito Akhilome <hovkard@gmail.com>
2019-05-10 18:59:10 +0100

    fix(build): fix build error
    [Finishes #165943708]

--- src/app/admin/dashboard/routes-overview/routes-overview.component.ts
+++ src/app/admin/dashboard/routes-overview/routes-overview.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RoutesOverviewComponent implements OnInit {
 
-  @Input() data: Object;
+  @Input() data: any;
   @Input() usage: string;
   constructor() { }
 
